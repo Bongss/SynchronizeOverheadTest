b350ee28042d7f2f2d4f4f50a290f3f4825842f5
bongki <bongki.cho07@gmail.com>
2018-06-13 01:02:22 +0900

    Add Synchrinized overhead test code

---
+++ src/sync/Table.java
@@ -0,0 +1,9 @@
+package sync;
+
+public abstract class Table {
+    public abstract int size();
+    public abstract void incrementEntry(int entryId, int inc);
+    public abstract int getData(int i);
+    public abstract void printTablename();
+
+}
